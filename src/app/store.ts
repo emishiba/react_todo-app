8c37132c854c864abe3f60a91b279ec2868d4533
emishiba <e.shibaoka@gmail.com>
2021-02-20 20:56:32 +0900

    local storageにデータ追加

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import taskReducer from '../features/task/taskSlice';
+import { save, load } from 'redux-localstorage-simple';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     task: taskReducer,
   },
+  preloadedState: load(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(save()),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/features/task/taskSlice.tsx
+++ src/features/task/taskSlice.tsx
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, RootState } from '../../app/store';
 
+
 export type TaskTypes = {
   id: number;
   title: string;
